f41ffc40555a5b573c780f1fb1c4afff4312c7c4
Daliloup <sanchezdali99@gmail.com>
2024-04-21 17:50:01 +0200

    Added Cdataframe prototypes

--- cdataframe.h
+++ cdataframe.h
@@ -9,10 +9,43 @@
 
 typedef list CDATAFRAME;
 
-CDATAFRAME make_cdataframe(ENUM_TYPE *cdftype, int size);
+
+// Basic Operations (Constructors, selectors, etc...)
+CDATAFRAME *create_cdataframe(int size);  // No type required in parameter
 void delete_cdataframe(CDATAFRAME **cdf);
+void add_empty_column(CDATAFRAME *cdf, char *col_name, ENUM_TYPE *cdftype);
 void delete_column(CDATAFRAME *cdf, char *col_name);
+void *get_value_cdataframe(CDATAFRAME *cdf, int col, int row);
+void *set_value_cdataframe(CDATAFRAME *cdf, int col, int row, void *new_value);
+void rename_col_cdataframe(CDATAFRAME *cdf, char *title, char *new_title);
+
+
+// Higher Order operations
 int get_cdataframe_cols_size(CDATAFRAME *cdf);
-void display_dataframe(CDATAFRAME *dataframe);
+int is_in_cdataframe(CDATAFRAME *cdf, void *value);
+
+
+// User Input
+void fill_cdataframe(CDATAFRAME *cdf);
+void add_column_user(CDATAFRAME *cdf);
+void add_row_cdataframe(CDATAFRAME *cdf);
+void delete_row_cdataframe(CDATAFRAME *cdf, int index);
+
+
+// Display
+void display_cdataframe(CDATAFRAME *cdf);
+void display_partial_cdataframe(CDATAFRAME *cdf);  // user inputs the section he wants to see
+void display_col_names(CDATAFRAME *cdf);
+void display_nb_rows(CDATAFRAME *cdf);
+void display_nb_columns(CDATAFRAME *cdf);
+void display_nb_equal_cdataframe(CDATAFRAME *cdf, void *value);
+void display_nb_greater_cdataframe(CDATAFRAME *cdf, void *value);
+void display_nb_less_cdataframe(CDATAFRAME *cdf, void *value);
+
+
+// CSV Saving and Loading
+CDATAFRAME* load_from_csv(char *file_name);
+void save_into_csv(CDATAFRAME *cdf, char *file_name);
+
 
 #endif

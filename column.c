f7f7a25e920be66048e30b4c66c2ab737a616c93
YannisBAHHH <150543425+YannisBAHHH@users.noreply.github.com>
2024-05-20 23:49:20 +0200

    Update column.c

--- column.c
+++ column.c
@@ -1,5 +1,8 @@
 //
 // Created by Yannis on 29/03/2024.
+// Project CDataFrame
+// Sanchez Dali, Benhamed--Aubry Yannis
+// This file contains all the functions related to the columns
 //
 
 #include "column.h"
